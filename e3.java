8b95854254790c397295667b814b1090ae2fbb26
Hiroakiship <83757835+Hiroakiship@users.noreply.github.com>
2022-07-14 21:44:12 +0900

    Update e3.java
    Final revision

--- e3.java
+++ e3.java
@@ -4,10 +4,13 @@ public class e3{
 
 public static void main(String[] args) {
 int h = 0;
-System.out.println("Tossing a coin...");
+Who w = new Who();
+String name = w.who(); 
 Random r = new Random();
 int a[] = new int[3];
-
+    
+System.out.println("Tossing a coin...");
+    
 for(int i = 0;i < 3;i++){
 a[i] = r.nextInt(2);
 if(a[i] == 0){
@@ -18,7 +21,8 @@ else System.out.printf("Round %d: Tails\n",i+1);
 }
 
 System.out.printf("Heads: %d, Tails: %d\n",h ,3-h);
-if(h > 3-h)System.out.println(""+ str +" won");
-else System.out.println(""+ str +" lost");
+    
+if(h > 3-h)System.out.println(""+ name +" won");
+else System.out.println(""+ name +" lost");
 }
 }
