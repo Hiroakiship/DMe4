cbb9fa0157c27ce87e7b38c25df6346e2b3b2a15
Hiroakiship <83757835+Hiroakiship@users.noreply.github.com>
2022-07-14 21:27:20 +0900

    Update Who.java
    User name added

--- Who.java
+++ Who.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Who{
   
-  public static void main(String[] args) {
+  public void who{
     System.out.println("Who are you");
     Scanner scan = new Scanner(System.in);
     String str = scan.next();
