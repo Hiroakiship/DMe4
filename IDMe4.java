13cd73d5d08c060d096d00b8e5d8763ce7996295
Hiroakiship <83757835+Hiroakiship@users.noreply.github.com>
2022-07-14 19:27:44 +0900

    Update and rename e3.java to IDMe4.java
    HoT: initial revision

---
+++ IDMe4.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class IDMe4{
+
+public static void main(String[] args) {
+int h = 0;
+System.out.println("Tossing a coin...");
+Random r = new Random();
+int a[] = new int[3];
+
+for(int i = 0;i < 3;i++){
+a[i] = r.nextInt(2);
+if(a[i] == 0){
+    System.out.printf("Round %d: Heads\n",i+1);
+h++;
+}
+else System.out.printf("Round %d: Tails\n",i+1);
+}
+
+System.out.printf("Heads: %d, Tails: %d\n",h ,3-h);
+}
+}
